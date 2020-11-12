f8a1a98cb0f0a3ef30e79abe61726e224fea8325
fran.dev <franco.dev99@gmail.com>
2020-11-12 02:50:46 -0300

    modifi component

---
+++ Front-End-App/src/Components/Home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css';
+
+function Home() {
+	return (
+		<div className="Home">
+			<header className="Home-header">
+				<h5>Fran Dev 99</h5>
+			</header>
+		</div>
+	);
+}
+
+export default Home;
