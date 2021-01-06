de7e0568ecfd1be9e4fc77804052639853b3aca1
Franco Garzón <57675375+FrancoGarzon99@users.noreply.github.com>
2021-01-06 10:41:22 -0300

    Feat(front-end-app/home) add buttons links

--- Front-End-App/src/Components/Home/Home.jsx
+++ Front-End-App/src/Components/Home/Home.jsx
@@ -1,13 +1,26 @@
 import './Home.css';
-
+import { Box, Button } from '@chakra-ui/core';
+import { Link } from 'react-router-dom';
 function Home() {
-	return (
-		<div className="Home">
-			<header className="Home-header">
-				<h5>Fran Dev 99</h5>
-			</header>
-		</div>
-	);
+  return (
+    <div className="Home">
+      <header className="Home-header">
+        <h5>Fran Dev 99</h5>
+        <Box>
+          <Link to="/iniciar-session">
+            <Button colorScheme="teal" variant="outline" m="10px">
+              Iniciar Session
+            </Button>
+          </Link>
+          <Link to="/registrarme">
+            <Button colorScheme="teal" variant="outline" m="10px">
+              Registrarme
+            </Button>
+          </Link>
+        </Box>
+      </header>
+    </div>
+  );
 }
 
 export default Home;
